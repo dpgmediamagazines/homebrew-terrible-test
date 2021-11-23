00679af555603f27012b7e18887d48c8e3cf08bb
Sebastiaan van der Vliet <sebastiaan.vandervliet@sanoma.com>
2021-11-23 15:00:15 +0100

    test

---
+++ terrible-m1.rb
@@ -0,0 +1,13 @@
+class Terriblem1 < Formula
+    homepage "https://terrible.dpgmm.tech/"
+    url "https://terrible.dpgmm.tech/download/terrible-darwin-m1.tar.gz"
+    version "2.71.0"
+    sha256 "c358445ffef62a9d35fe5d90d8499b250f9f8f838e7ab64a235f2b508e6ca519"
+    #sha256 "c358445ffef62a9d35fe5d90d8499b250f9f8f838e7ab64a235f2b508e6ca519"
+  
+    def install
+        bin.install "terrible-m1"
+        ohai "The terrible client is installed on your machine. Run `terrible --help` for usage info, or read the online docs."
+    end
+  end
+  
