32a2356302ef7133a59323debeef9770f39498dc
Bamboo <bamboo@bamboo>
2022-01-21 10:05:12 +0000

    Bump to version c44479a14b18e191b8ddaac48c19a97164c44dbe

--- terrible-m1.rb
+++ terrible-m1.rb
@@ -1,9 +1,9 @@
 class TerribleM1 < Formula
     homepage "https://terrible.test.dpgmm.tech/"
     url "https://terrible.test.dpgmm.tech/download/terrible-darwin-m1.tar.gz"
-    version "dfa52363b36e60ca7139ad159bb5f00e91a6fada"
-    sha256 "b4325be03051dc13a96500beab01f52f3cb8b9ec0e0aedc533691c80e05fb059"
-    #sha256 "b4325be03051dc13a96500beab01f52f3cb8b9ec0e0aedc533691c80e05fb059"
+    version "c44479a14b18e191b8ddaac48c19a97164c44dbe"
+    sha256 "199c2f00cca341012ae13235cb4a160ee4c6f674ed2b91a401cdfd3130a292ff"
+    #sha256 "199c2f00cca341012ae13235cb4a160ee4c6f674ed2b91a401cdfd3130a292ff"
   
     def install
         bin.install "terrible-m1"
--- terrible.rb
+++ terrible.rb
@@ -1,9 +1,9 @@
 class Terrible < Formula
   homepage "https://terrible.test.dpgmm.tech/"
   url "https://terrible.test.dpgmm.tech/download/terrible-darwin.tar.gz"
-  version "dfa52363b36e60ca7139ad159bb5f00e91a6fada"
-  sha256 "cfe3a6b89a0f579d0060272dd052fc07ffb0a9163c218893fcbdd9a32ea92ec2"
-  #sha256 "cfe3a6b89a0f579d0060272dd052fc07ffb0a9163c218893fcbdd9a32ea92ec2"
+  version "c44479a14b18e191b8ddaac48c19a97164c44dbe"
+  sha256 "6f1095633f28517f1157631b8d39a1cc60dfaf2278fdd64b993bf66fee1fd1f6"
+  #sha256 "6f1095633f28517f1157631b8d39a1cc60dfaf2278fdd64b993bf66fee1fd1f6"
 
   def install
       bin.install "terrible"
